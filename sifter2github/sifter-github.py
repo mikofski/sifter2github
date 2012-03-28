80049c2f3c34160c15efefc660b4b1c287f27874
Mark Mikofski <bwanamarko@yahoo.com>
2012-03-28 00:28:07 -0700

    remove tabs

--- sifter2github/sifter-github.py
+++ sifter2github/sifter-github.py
@@ -1,11 +1,15 @@
 import requests, json
 
 class Sifter(Object):
+    """
+    Reads issues from Sifter using sifterapi
+    """
     def __init__(self, domain, accessKey):
         sifterURL = 'https://'+domain+'.sifterapp.com/api/projects'
         r = requests.get(sifterURL,
                          headers={'X-Sifter-Token':accessKey,
                          'Accept':'application/json'})
-        projects = json.loads(r.content);
+        allProjects = json.loads(r.content);
+        projects = allProjects['projects']
         for p in projects:
             
--- sifter2github/sifter2github.py
+++ sifter2github/sifter2github.py
@@ -25,13 +25,13 @@
 
 
 def sifter2github(path_to_csv, username, token, repository, path_to_py_github='~/Downloads/py-github'):
-	from import_from_csv import *
-	s = SifterIssues(path_to_csv)
-	g = GithubRepo(username, token, repository)
-	import_issues(g, s)
-	return 0
+    from import_from_csv import *
+    s = SifterIssues(path_to_csv)
+    g = GithubRepo(username, token, repository)
+    import_issues(g, s)
+    return 0
 
 if __name__ == '__main__':
-	import sys
-	sys.path.append(path_to_py_github)
+    import sys
+    sys.path.append(path_to_py_github)
     sifter2github(*sys.argv[1:]) # sys.argv[0] is the module name
