3d8c083ead31499f323a437783b739b9fe8540c2
Mark Mikofski <bwanamarko@yahoo.com>
2012-03-31 01:28:18 -0700

    implement sifter-python in sifter2github * import Sifter class * call py-Github with Sifter fieldnames * TODO remote py-githun and just use api v3 with requests * TODO put milestones in milestones, not labels * TODO add comments to comments

--- sifter2github/import_from_csv.py
+++ sifter2github/import_from_csv.py
@@ -1,6 +1,7 @@
 from github import github
 import csv
 import time
+from sifter import Sifter
 
 class SifterIssues(object):
     """
@@ -16,9 +17,9 @@ class SifterIssues(object):
                 issues.append(row)
             self.issues = sorted(issues,key=lambda k: k['Number'])
         elif type(path_to_csv) in set([list,tuple]) and len(path_to_csv) == 3:
-            self.issues = sifter.Sifter(*path_to_csv)
+            self.issues = Sifter(*path_to_csv)
         elif type(path_to_csv) == dict and len(path_to_csv) == 3:
-            self.issues = sifter.Sifter(**path_to_csv)
+            self.issues = Sifter(**path_to_csv)
 
 class GithubRepo(object):
     """
@@ -32,22 +33,37 @@ class GithubRepo(object):
 
 
 def import_issues(gh, sifter):
-    for i in sifter.issues:
-        print "Importing sifter issue #", i['Number'],"...",
-        subject = i['Subject'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
-        descr = i['Description'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
-        descr = "---Migrated from Sifter - Sifter issue " + str(i['Number']) + "---\n" + descr
-        milestone = i['Milestone'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
-        category = i['Category'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
-        new_issue = gh.account.issues.new(gh.user, gh.repo, subject, descr)
-        if milestone:
-            gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, milestone)
-        if category:
-            gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, category)
-        if i['Status'] == "Closed" or i['Status'] == "Resolved":
-            gh.account.issues.close(gh.user, gh.repo, new_issue.number)
-        print "done. Github issue #", new_issue.number
-        time.sleep(2)
-
-
-        
+    if type(sifter) == SifterIssues:
+        for i in sifter.issues:
+            print "Importing sifter issue #", i['Number'],"...",
+            subject = i['Subject'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            descr = i['Description'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            descr = "---Migrated from Sifter - Sifter issue " + str(i['Number']) + "---\n" + descr
+            milestone = i['Milestone'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            category = i['Category'].replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            new_issue = gh.account.issues.new(gh.user, gh.repo, subject, descr)
+            if milestone:
+                gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, milestone)
+            if category:
+                gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, category)
+            if i['Status'] == "Closed" or i['Status'] == "Resolved":
+                gh.account.issues.close(gh.user, gh.repo, new_issue.number)
+            print "done. Github issue #", new_issue.number
+            time.sleep(2)
+    elif type(sifter) == Sifter:
+        for i in sifter.issues:
+            print "Importing sifter issue #", i.number,"...",
+            subject = i.subject.replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            descr = i.description.replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            descr = "---Migrated from Sifter - Sifter issue " + str(i.number) + "---\n" + descr
+            milestone = i.milestone_name.replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            category = i.category_name.replace("\\","_").replace("'","-").replace("\"","-").replace("/","-").replace(":","-").replace(",","-")
+            new_issue = gh.account.issues.new(gh.user, gh.repo, subject, descr)
+            if milestone:
+                gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, milestone)
+            if category:
+                gh.account.issues.add_label(gh.user, gh.repo, new_issue.number, category)
+            if i.status == "Closed" or i.status == "Resolved":
+                gh.account.issues.close(gh.user, gh.repo, new_issue.number)
+            print "done. Github issue #", new_issue.number
+            time.sleep(2)
