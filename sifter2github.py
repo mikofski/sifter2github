2db76fabce155f1d6430767cd272c2d867cb57dc
Jeff Roche <jeff.roche@gmail.com>
2012-04-30 16:56:59 -0700

    Fixes #5

--- sifter2github.py
+++ sifter2github.py
@@ -14,7 +14,7 @@ class SifterIssues(object):
         proj_names = [p.name for p in _projects]
         _project = _projects[proj_names.index(project)]
         self.issues = _project.issues()
-        self.comments = [_issue.comments() for _issue in self.issues]
+        self.comments = [sorted(_issue.comments(), key=lambda comment: comment.created_at) for _issue in self.issues]
         self.milestones = _project.milestones()
         self.users = _project.people()
         print len(self.issues), "issues loaded."
